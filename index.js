2d943c4fa170f2d015b1caea6b54659c8ac30e47
Alejandro Mazas de Lizana <alejandro.mazasdelizana@gmail.com>
2022-09-05 22:13:17 +0200

    static files

--- index.js
+++ index.js
@@ -10,14 +10,17 @@
 // console.log(`Server on port ${3000}`)
 
 const express = require('express')
+const morgan = require('morgan')
 
 const app = express()
 
-app.use((req, res, next) => {
-    console.log('paso por aqui')
-    console.log(`Route ${req.url} Metodo ${req.method}`)
-    next()
+app.use(express.json())
+app.use(morgan('dev'))
+
+app.get('/note.txt', (req, res) => {
+    res.send('este no es un archivo')
 })
+
 
 app.get('/profile', (req, res) => {
     res.send('profile page')
@@ -28,18 +31,27 @@ app.all('/about', (req, res) => {
 })
 
 app.use((req, res, next) => {
-    if (req.query.login === 'alejandro@gmail.com') {
-        next()
-    } else
-        res.send('no autorizado')
+    console.log('paso por aqui')
+    console.log(`Route ${req.url} Metodo ${req.method}`)
     next()
 })
+
+//middleware de auth
+
+// app.use((req, res, next) => {
+//     if (req.query.login === 'alejandro@gmail.com') {
+//         next()
+//     } else
+//         res.send('no autorizado')
+//     next()
+// })
 
 app.get('/dashboard', (req, res) => {
     res.send('dasboard page')
 })
 
+app.use('/public', express.static('./public'))
+app.use('/uploads', express.static('./uploads'))
 
 app.listen(3000)
-
 console.log(`Server on port ${3000}`)
